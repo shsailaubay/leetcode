9ae6716db0b37d3f072cf2d63aebbc2653287162
Sailaubai Shyngys <shsailaubay@gmail.com>
2023-05-17 23:59:01 +0600

    Time: 728 ms (75.13%) | Memory: 29.4 MB (11.56%) - LeetSync

--- container-with-most-water/container-with-most-water.py
+++ container-with-most-water/container-with-most-water.py
@@ -3,8 +3,11 @@ class Solution:
         mx = 0
         i = 0
         j = len(height) - 1
+        height_max = max(height)
         while i < j:
             mx = max(mx, min(height[i], height[j]) * (j - i))
+            if height_max * (j - i) == mx:
+                break
             if height[i] < height[j]:
                 i += 1
             else:
