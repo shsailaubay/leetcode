ad5852b296c3110a647a551eddfebc309e8f21d2
Sailaubai Shyngys <shsailaubay@gmail.com>
2023-05-17 20:20:16 +0600

    Time: 106 ms (27.40%) | Memory: 16.5 MB (23.21%) - LeetSync

--- median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -2,22 +2,11 @@ from collections import deque
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        len_sum = len(nums1) + len(nums2)
-        to = (len_sum // 2) + 1
-        center = None
-        if len_sum % 2 == 0:
-            to -= 1
-        medians = deque(maxlen=2 if len_sum % 2 == 0 else 1)
-        for i in range(len_sum - 1, to - 2, -1):
-            if nums1 and nums2:
-                if nums1[-1] > nums2[-1]:
-                    medians.append(nums1.pop())
-                else:
-                    medians.append(nums2.pop())
-            elif nums1:
-                medians.append(nums1.pop())
-            else:
-                medians.append(nums2.pop())
-        return sum(medians) / len(medians)
+        nums1.extend(nums2)
+        nums1.sort()
+        if len(nums1) % 2 == 0:
+            return (nums1[len(nums1) // 2] + nums1[len(nums1) // 2 - 1]) / 2
+        else:
+            return nums1[len(nums1) // 2]
 
         
