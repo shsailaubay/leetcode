8221633c0e445e7970d2a6573d2071ca9f8f57fb
Sailaubai Shyngys <shsailaubay@gmail.com>
2023-05-21 02:35:54 +0600

    Time: 2609 ms (81.81%) | Memory: 55.1 MB (56.22%) - LeetSync

--- count-primes/count-primes.py
+++ count-primes/count-primes.py
@@ -1,19 +1,18 @@
 from math import sqrt, ceil
-from array import array
 
 class Solution:
 
     def countPrimes(self, n: int) -> int:
         if n <= 2:
             return 0
-        primes = array('H', [1] * n)
-        primes[0] = primes[1] = 0 
+        primes = [True] * n
+        primes[0] = primes[1] = False 
 
         for i in range(2, ceil(sqrt(n))):
 
             if primes[i]:
                 for j in range(i * i, n, i):
-                    primes[j] = 0
+                    primes[j] = False
         
         return sum(primes)
 
