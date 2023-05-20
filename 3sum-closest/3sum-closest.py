6ae72a80f82b2fe8d2711f7f5c75dde5af63dbc8
Sailaubai Shyngys <shsailaubay@gmail.com>
2023-05-20 19:48:09 +0600

    Time: 1424 ms (8.65%) | Memory: 16.6 MB (10.96%) - LeetSync

---
+++ 3sum-closest/3sum-closest.py
@@ -0,0 +1,49 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        if target >= 3000:
+            return nums[-1] + nums[-2] + nums[-3]
+        elif target <= -3000:
+            return nums[0] + nums[1] + nums[2]
+
+        def diff(a, b):
+            return abs(a - b)
+        
+        def bins(s: int, lst: List[int]) -> int:
+            if len(lst) == 1:
+                return lst[0]
+            center = len(lst) // 2
+            el = lst[center]
+            d = target - s + el
+            if d == 0:
+                return lst[center]
+            if s + lst[center] > target:
+                lst = lst[0: center]
+            else:
+                lst = lst[center: len(lst)]
+            if len(lst) == 0:
+                return el
+            return bins(s, lst)
+
+        closest = None
+        _c_diff = None
+        for i in range(0, len(nums)):
+            for j in range(i+1, len(nums)):
+                s = nums[i] + nums[j]
+                lst = nums[i + 1:j] + nums[j+1:len(nums)]
+                if len(lst) == 0:
+                    continue
+                _new_cl = bins(s, lst)
+                _new_diff = diff(target, s + _new_cl)
+
+                if closest == None:
+                    closest = s + _new_cl
+                    _c_diff = _new_diff
+                if _new_diff == 0:
+                    return s + _new_cl
+                if _new_diff < _c_diff:
+                    closest = s + _new_cl
+                    _c_diff = _new_diff
+    
+        return closest
+                
