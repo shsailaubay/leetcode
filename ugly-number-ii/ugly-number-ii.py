b0beea805f299dc451d3a383e133762d04a4fc5b
Sailaubai Shyngys <shsailaubay@gmail.com>
2023-05-20 23:41:46 +0600

    Time: 256 ms (19.63%) | Memory: 16.7 MB (9.65%) - LeetSync

--- ugly-number-ii/ugly-number-ii.py
+++ ugly-number-ii/ugly-number-ii.py
@@ -1,13 +1,19 @@
+from heapq import heapify, heappush, heappop
+
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
+        res = 1
         s = set()
-        minset = {1,2,3}
+        minset = [1,2,3]
+        heapify(minset)
         while len(s) < n:
-            _m = min(minset)
-            minset.remove(_m)
+            _m = heappop(minset)
             if _m in s:
                 continue
             s.add(_m)
-            minset.update([_m * 2, _m * 3, _m * 5])
-        return list(sorted(s))[n - 1]
+            res = _m
+            heappush(minset, _m * 2)
+            heappush(minset, _m * 3)
+            heappush(minset, _m * 5)
+        return res
         
