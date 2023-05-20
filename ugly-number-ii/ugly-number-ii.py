510ff7d79ad1254bbfdacd4a7f7cdff80b3fabc5
Sailaubai Shyngys <shsailaubay@gmail.com>
2023-05-20 23:29:46 +0600

    Time: 613 ms (7.82%) | Memory: 16.7 MB (9.65%) - LeetSync

--- ugly-number-ii/ugly-number-ii.py
+++ ugly-number-ii/ugly-number-ii.py
@@ -4,8 +4,10 @@ class Solution:
         minset = {1,2,3}
         while len(s) < n:
             _m = min(minset)
-            s.add(_m)
             minset.remove(_m)
+            if _m in s:
+                continue
+            s.add(_m)
             minset.update([_m * 2, _m * 3, _m * 5])
         return list(sorted(s))[n - 1]
         
