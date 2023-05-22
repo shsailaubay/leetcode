72c599d539f23c738d882dc7ce33c36d9c6c2ec6
Sailaubai Shyngys <shsailaubay@gmail.com>
2023-05-22 19:03:26 +0600

    Time: 52 ms (22.82%) | Memory: 16.5 MB (21.01%) - LeetSync

--- evaluate-division/evaluate-division.py
+++ evaluate-division/evaluate-division.py
@@ -2,17 +2,20 @@ from collections import defaultdict
 
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        d = defaultdict(defaultdict)
+        d = defaultdict(dict)
+
         for i in range(0, len(equations)):
             a, b = equations[i]
-            d[a][b] = values[i]
-            for e, itm in d.items():
-                if a in itm and b not in itm:
-                    itm[b] = itm[a] * values[i]
-                if b in itm and a not in itm:
-                    itm[a] = itm[b] / values[i]
+            val = values[i]
 
-            d[b][a] = 1 / values[i]
+            d[a][b] = val
+            d[b][a] = 1 / val
+
+            for e, itm in d.items():
+                if a in itm:
+                    itm[b] = itm[a] * values[i]
+                if b in itm:
+                    itm[a] = itm[b] / values[i]
 
         for a in d:
             keys = list(d[a].keys())
