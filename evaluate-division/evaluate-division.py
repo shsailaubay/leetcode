e6659f31fe83238884b0195c0d2bf7e2af3b469c
Sailaubai Shyngys <shsailaubay@gmail.com>
2023-05-22 21:46:12 +0600

    Time: 58 ms (6.87%) | Memory: 16.4 MB (21.01%) - LeetSync

--- evaluate-division/evaluate-division.py
+++ evaluate-division/evaluate-division.py
@@ -2,17 +2,19 @@ from collections import defaultdict
 
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        d = defaultdict(dict)
+        d = dict()
         visited = set()
 
         def add(a, b, val):
             if (a, b) not in visited:
+                if a not in d:
+                    d[a] = dict()
                 visited.add((a, b))
                 d[a][b] = val
 
                 for i in list(d.keys()):
                     if a in d[i]:
-                        add(i, b, d[i][a] * val)
+                        d[i][b] = d[i][a] * val
                 
                 if b in d:
                     for k, v in list(d[b].items()):
@@ -24,33 +26,14 @@ class Solution:
 
             add(a, b, val)
             add(b, a, 1 / val)
-        
-        print(d)
-
-            # for e, itm in d.items():
-            #     if a in itm:
-            #         itm[b] = itm[a] * values[i]
-            #     if b in itm:
-            #         itm[a] = itm[b] / values[i]
-
-        # for a in d:
-        #     keys = list(d[a].keys())
-        #     for k in keys:
-        #         v = d[a][k]
-        #         for b, bval in d[k].items():
-        #             if b in d[a] or b == a:
-        #                 continue
-        #             d[a][b] = bval * v
 
         res = []
         for a, b in queries:
             if a not in d:
                 res.append(-1)
                 continue
-            if a == b:
-                res.append(1)
-                continue 
             res.append(d[a].get(b, -1))
+
         return res
             
 
