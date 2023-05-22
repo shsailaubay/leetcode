09fa7cbc3dadb72b960c7223b3e0501844b14ca5
Sailaubai Shyngys <shsailaubay@gmail.com>
2023-05-22 21:42:56 +0600

    Time: 51 ms (26.02%) | Memory: 16.7 MB (21.01%) - LeetSync

--- evaluate-division/evaluate-division.py
+++ evaluate-division/evaluate-division.py
@@ -3,28 +3,44 @@ from collections import defaultdict
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         d = defaultdict(dict)
+        visited = set()
+
+        def add(a, b, val):
+            if (a, b) not in visited:
+                visited.add((a, b))
+                d[a][b] = val
+
+                for i in list(d.keys()):
+                    if a in d[i]:
+                        add(i, b, d[i][a] * val)
+                
+                if b in d:
+                    for k, v in list(d[b].items()):
+                        add(a, k, v * val) 
 
         for i in range(0, len(equations)):
             a, b = equations[i]
             val = values[i]
 
-            d[a][b] = val
-            d[b][a] = 1 / val
+            add(a, b, val)
+            add(b, a, 1 / val)
+        
+        print(d)
 
-            for e, itm in d.items():
-                if a in itm:
-                    itm[b] = itm[a] * values[i]
-                if b in itm:
-                    itm[a] = itm[b] / values[i]
+            # for e, itm in d.items():
+            #     if a in itm:
+            #         itm[b] = itm[a] * values[i]
+            #     if b in itm:
+            #         itm[a] = itm[b] / values[i]
 
-        for a in d:
-            keys = list(d[a].keys())
-            for k in keys:
-                v = d[a][k]
-                for b, bval in d[k].items():
-                    if b in d[a] or b == a:
-                        continue
-                    d[a][b] = bval * v
+        # for a in d:
+        #     keys = list(d[a].keys())
+        #     for k in keys:
+        #         v = d[a][k]
+        #         for b, bval in d[k].items():
+        #             if b in d[a] or b == a:
+        #                 continue
+        #             d[a][b] = bval * v
 
         res = []
         for a, b in queries:
