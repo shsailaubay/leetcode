0b81fb85bc4451f93d0840916ba0725c0f97e0d3
Sailaubai Shyngys <shsailaubay@gmail.com>
2023-05-28 01:09:09 +0600

    Time: 105 ms (11.53%) | Memory: 16.6 MB (48.43%) - LeetSync

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,43 +1,11 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def search(lst: List[int], startIndex: int) -> int:
-            if len(lst) == 0:
-                return -1
-            center = len(lst) // 2
-            if lst[center] == target:
-                return startIndex + center
-            elif lst[center] > target:
-                return search(lst[:center], startIndex)
-            return search(lst[center + 1:], center + startIndex + 1)
-        
-        index = search(nums, 0)
-
-        if index == -1:
+        if len(nums) == 0:
             return [-1, -1]
-        
-        i = j = index
+        left = bisect.bisect_left(nums, target)
 
-        i_c = j_c = False
+        if left >= len(nums) or nums[left] != target:
+            return [-1, -1]
+        right = bisect.bisect_right(nums, target)
 
-        while i_c == False or j_c == False:
-
-            if i - 1 >= 0:
-                p = nums[i - 1]
-                if p == target:
-                    i -= 1
-                else:
-                    i_c = True
-            else:
-                i_c = True
-
-            if j + 1 < len(nums):            
-                n = nums[j + 1]
-
-                if n == target:
-                    j += 1
-                else:
-                    j_c = True
-            else:
-                j_c = True
-        
-        return [i, j]
+        return [left, right - 1]
