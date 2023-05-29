1dd1eb4d55f7a57fa813f4e672bf78f1eb8ca967
Sailaubai Shyngys <shsailaubay@gmail.com>
2023-05-30 00:01:43 +0600

    Time: 812 ms (36.33%) | Memory: 40.9 MB (42.21%) - LeetSync

--- alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.py
+++ alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.py
@@ -4,7 +4,6 @@ from bisect import insort
 class Solution:
     def alertNames(self, keyName: List[str], keyTime: List[str]) -> List[str]:
 
-        @cache
         def to_int(t) -> int:
             h, m = t.split(":")
             return (int(h) * 60) + int(m)
@@ -12,30 +11,17 @@ class Solution:
         names = defaultdict(list)
 
         for i in range(0, len(keyName)):
-            insort(names[keyName[i]], to_int(keyTime[i]))
-
-        @cache
-        def in_one_hour(a, b) -> bool:
-            # if a > 1380 and b < 60:
-            #     b += 1440
-            
-            return 0 <= b - a <= 60
+            names[keyName[i]].append(to_int(keyTime[i]))
         
         res = []
 
         for k, val in names.items():
-
-            # if val[-1] > 1380:
-            #     if val[0] < 60:
-            #         val.append(val[0])
-            #     if val[1] < 60:
-            #         val.append(val[1])
             
             if len(val) < 3:
                 continue
-            
+            val.sort()
             for i in range(0, len(val) - 2):
-                if in_one_hour(val[i], val[i + 2]):
+                if 0 <= val[i + 2] - val[i] <= 60:
                     res.append(k)
                     break
         
