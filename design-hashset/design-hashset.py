b47bb705fdd9e1ee76fe6c5d2b195173d53afd5a
Sailaubai Shyngys <shsailaubay@gmail.com>
2023-05-30 23:43:19 +0600

    Time: 177 ms (60.37%) | Memory: 20.7 MB (61.30%) - LeetSync

--- design-hashset/design-hashset.py
+++ design-hashset/design-hashset.py
@@ -3,13 +3,15 @@ from array import array
 class MyHashSet:
 
     def __init__(self):
-        self.list = [False] * 1001
+        self.list = [False]
         
     def _get_bucket(self, key: int):
         return divmod(key, 1000)
 
     def add(self, key: int) -> None:
         i, b = self._get_bucket(key)
+        if i >= len(self.list):
+            self.list.extend([False] * (i - len(self.list) + 1))
         if self.list[i] == False:
             self.list[i] = array('B', [0] * 1001)
         self.list[i][b] = 1
@@ -17,13 +19,13 @@ class MyHashSet:
 
     def remove(self, key: int) -> None:
         i, b = self._get_bucket(key)
-        if self.list[i] != False:
+        if i < len(self.list) and self.list[i] != False:
             self.list[i][b] = 0
         
 
     def contains(self, key: int) -> bool:
         i, b = self._get_bucket(key)
-        if self.list[i] == False:
+        if i >= len(self.list) or self.list[i] == False:
             return False
         return self.list[i][b]
 
