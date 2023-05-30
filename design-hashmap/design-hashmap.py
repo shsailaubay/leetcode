1e887ba418fd2b879016878d136c11aab072f33c
Sailaubai Shyngys <shsailaubay@gmail.com>
2023-05-31 00:09:22 +0600

    Time: 236 ms (72.00%) | Memory: 19.6 MB (45.34%) - LeetSync

--- design-hashmap/design-hashmap.py
+++ design-hashmap/design-hashmap.py
@@ -3,17 +3,35 @@ from array import array
 class MyHashMap:
 
     def __init__(self):
-        self.table = [-1] * ((10 ** 6) + 1)
+        self.table = []
+    
+    def _coord(self, key: int) -> tuple[int, int]:
+        return divmod(key, 1000)
 
     def put(self, key: int, value: int) -> None:
-        self.table[key] = value
+        r, c = self._coord(key)
+        if r >= len(self.table):
+            self.table.extend([-1] * (r - len(self.table) + 1))
+        if self.table[r] == -1:
+            self.table[r] = array("i", [-1] * 1001)
+        self.table[r][c] = value
         
 
     def get(self, key: int) -> int:
-        return self.table[key]
+        r, c = self._coord(key)
+        if len(self.table) <= r:
+            return -1
+        if self.table[r] == -1:
+            return -1
+        return self.table[r][c]
 
     def remove(self, key: int) -> None:
-        self.put(key, -1)
+        r, c = self._coord(key)
+        if len(self.table) <= r:
+            return
+        if self.table[r] == -1:
+            return
+        self.table[r][c] = -1
 
 
 # Your MyHashMap object will be instantiated and called as such:
