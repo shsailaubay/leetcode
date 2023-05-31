a4e49b80b3952d2e06e6fc03bc0d8240e55b6b05
Sailaubai Shyngys <shsailaubay@gmail.com>
2023-05-31 15:09:03 +0600

    Time: 259 ms (26.03%) | Memory: 26.4 MB (41.64%) - LeetSync

--- design-underground-system/design-underground-system.py
+++ design-underground-system/design-underground-system.py
@@ -10,10 +10,10 @@ class UndergroundSystem:
     def _setDirectionValue(self, startStation: str, endStation: str, t: int) -> None:
         key = (startStation, endStation)
         if key not in self.completed:
-            self.completed[key] = (t, 1)
+            self.completed[key] = [t, 1]
         else:
-            v, c = self.completed[key]
-            self.completed[key] = (v + ((t - v)/(c + 1)), c + 1)
+            self.completed[key][0] += t
+            self.completed[key][1] += 1
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
         self.checkins[id] = (stationName, t)
@@ -23,8 +23,15 @@ class UndergroundSystem:
         self._setDirectionValue(checkinStation, stationName, t - checkinTime)
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        return self.completed[(startStation, endStation)][0]
+        return self.completed[(startStation, endStation)][0] / self.completed[(startStation, endStation)][1]
+ 
 
+
+# Your UndergroundSystem object will be instantiated and called as such:
+# obj = UndergroundSystem()
+# obj.checkIn(id,stationName,t)
+# obj.checkOut(id,stationName,t)
+# param_3 = obj.getAverageTime(startStation,endStation)
 
 # Your UndergroundSystem object will be instantiated and called as such:
 # obj = UndergroundSystem()
