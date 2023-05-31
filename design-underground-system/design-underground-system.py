4e3d7427dd9fc0a399158a0fe94e75ff81c984fc
Sailaubai Shyngys <shsailaubay@gmail.com>
2023-05-31 15:11:01 +0600

    Time: 262 ms (20.98%) | Memory: 26.5 MB (27.44%) - LeetSync

--- design-underground-system/design-underground-system.py
+++ design-underground-system/design-underground-system.py
@@ -1,38 +1,23 @@
-from statistics import mean
+from collections import defaultdict
 
 class UndergroundSystem:
 
     def __init__(self):
         # key = current cutomer id, value = tuple(station_name, t)
         self.checkins = dict()
-        self.completed = dict()
-    
-    def _setDirectionValue(self, startStation: str, endStation: str, t: int) -> None:
-        key = (startStation, endStation)
-        if key not in self.completed:
-            self.completed[key] = [t, 1]
-        else:
-            self.completed[key][0] += t
-            self.completed[key][1] += 1
+        self.completed = defaultdict(lambda: [0, 0])
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
         self.checkins[id] = (stationName, t)
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
         checkinStation, checkinTime = self.checkins.pop(id)
-        self._setDirectionValue(checkinStation, stationName, t - checkinTime)
+        self.completed[(checkinStation, stationName)][0] += t - checkinTime
+        self.completed[(checkinStation, stationName)][1] += 1
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
         return self.completed[(startStation, endStation)][0] / self.completed[(startStation, endStation)][1]
  
-
-
-# Your UndergroundSystem object will be instantiated and called as such:
-# obj = UndergroundSystem()
-# obj.checkIn(id,stationName,t)
-# obj.checkOut(id,stationName,t)
-# param_3 = obj.getAverageTime(startStation,endStation)
-
 # Your UndergroundSystem object will be instantiated and called as such:
 # obj = UndergroundSystem()
 # obj.checkIn(id,stationName,t)
