30d4d5a76d4eb4b99ae62d14d8adcda1e4d8ee30
Sailaubai Shyngys <shsailaubay@gmail.com>
2023-06-02 00:05:38 +0600

    Time: 122 ms (36.61%) | Memory: 20.3 MB (36.63%) - LeetSync

---
+++ group-anagrams/group-anagrams.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = defaultdict(list)
+
+        for i in strs:
+            d["".join(sorted(i))].append(i)
+        
+        return d.values()
