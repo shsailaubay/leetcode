e6483f608fc87844f9fea30b49549899123b1dda
Sailaubai Shyngys <shsailaubay@gmail.com>
2023-06-12 12:40:33 +0600

    Time: 133 ms (33.56%) | Memory: 17.7 MB (74.92%) - LeetSync

--- majority-element-ii/majority-element-ii.py
+++ majority-element-ii/majority-element-ii.py
@@ -1,7 +1,14 @@
-from collections import Counter
+from collections import defaultdict
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         t = int(len(nums) / 3)
-        count = Counter(nums)
-        return [e for e, c in count.items() if c > t]
+        d = defaultdict(lambda: 0)
+        res = set()
+
+        for n in nums:
+            d[n] += 1
+            if d[n] > t:
+                res.add(n)
+        
+        return res
